29f80c6f9171b2ea76356f8bfed31132ba51f5f8
StanislasE <stanislas.etienne@gmail.com>
2020-11-19 10:28:25 +0100

    fait

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 2</title>
+</head>
+<body>
+<p>
+<?php
+    $month = array('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre') ;
+
+    echo $month[4] ;
+?>
+</p>
+
+</body>
+</html>
